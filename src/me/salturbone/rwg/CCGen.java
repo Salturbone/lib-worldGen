545fb339622f5929fd07055ac322eeb60e130da3
Salturbone <salturbone@gmail.com>
2019-09-08 19:31:08 +0300

    bijiler

--- src/me/salturbone/rwg/CCGen.java
+++ src/me/salturbone/rwg/CCGen.java
@@ -8,11 +8,12 @@ import org.bukkit.block.Biome;
 import org.bukkit.generator.ChunkGenerator;
 
 import me.salturbone.rwg.biomes.BiOcean;
+import me.salturbone.rwg.lib_noise.SimplexNoiseGenerate;
 
 public class CCGen extends ChunkGenerator {
 
 
-    public static int minHeight = 61;
+    public static int minHeight = 50;
     public CCGen() {
         
     }
@@ -21,12 +22,19 @@ public class CCGen extends ChunkGenerator {
     public ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, BiomeGrid biome) {
         ChunkData chunk = createChunkData(world);
         //Biomes
-        BiOcean ocean_biome = new BiOcean(world.getSeed(), 0.0005D, 0.004D, chunk, biome);
+
+        SimplexNoiseGenerate imsimplex = new SimplexNoiseGenerate(world.getSeed());
+        imsimplex.setScale(0.0005D);
+        BiOcean ocean_biome = new BiOcean(imsimplex, 0.004D, chunk, biome);
 
 
         int currentHeight = minHeight;
         for (int X = 0; X < 16; X++) {
             for (int Z = 0; Z < 16; Z++) {
+                currentHeight = (int) ((imsimplex.noise(chunkX * 16 + X,chunkZ * 16 + Z) + 1) * 15D + minHeight);
+                
+                if (currentHeight >= 62) currentHeight = 61;
+                
                 //FLAT GENERATOR
                 biome.setBiome(X, Z, Biome.PLAINS);
                 chunk.setBlock(X, currentHeight, Z, Material.GRASS);
@@ -38,7 +46,7 @@ public class CCGen extends ChunkGenerator {
                 }
                 chunk.setBlock(X, 0, Z, Material.BEDROCK);
 
-
+                //BIOME GENERATOR
                 currentHeight = calculateMaxHeight(X, Z, chunk);
                 ocean_biome.draw(X, currentHeight, Z, chunkX, chunkZ);
             }
--- src/me/salturbone/rwg/biomes/BiOcean.java
+++ src/me/salturbone/rwg/biomes/BiOcean.java
@@ -10,19 +10,23 @@ import me.salturbone.rwg.lib_noise.SimplexNoiseGenerate;
 
 public class BiOcean {
     long m_seed;
-    SimplexNoiseGenerate biome_noise, draw_noise;
+    SimplexNoiseGenerate biome_noise, draw_noise, d_draw_noise;
     ChunkData chunk;
     BiomeGrid biome;
     int ocean_limit = 61;
-    public BiOcean(long seed, double biome_scale, double draw_scale, ChunkData chunk_data, BiomeGrid biome_data) {
+    int deep_ocean_limit;
+    public BiOcean(SimplexNoiseGenerate sng,double draw_scale, ChunkData chunk_data, BiomeGrid biome_data) {
 
-        m_seed = seed;
+        m_seed = sng.getSeed();
 
-        biome_noise = new SimplexNoiseGenerate(seed);
-        biome_noise.setScale(biome_scale);
+        biome_noise = sng;
+        biome_noise.setScale(draw_scale / 2);
 
-        draw_noise = new SimplexNoiseGenerate(seed);
-        draw_noise.setScale(draw_scale);
+        draw_noise = new SimplexNoiseGenerate(m_seed);
+        draw_noise.setScale(draw_scale / 2);
+
+        d_draw_noise = new SimplexNoiseGenerate(m_seed);
+        d_draw_noise.setScale(draw_scale*10);
 
         chunk = chunk_data;
         biome = biome_data;
@@ -30,29 +34,32 @@ public class BiOcean {
     }
 
     public void draw(int x, int y, int z, int chunk_x, int chunk_z) {
-        int h = 0;
-        double c_noise = biome_noise.noise(chunk_x * 16 + x, chunk_z * 16 + z);
+        int h = y;
         double d_noise = draw_noise.noise(chunk_x * 16 + x, chunk_z * 16 + z);
-        if (c_noise < -0.1 && c_noise >= -0.15) {
-            biome.setBiome(x,z,Biome.OCEAN);
-            h = (int) (y + (-1 * (Math.abs(d_noise) * 13)) + 3);
+        if (h < ocean_limit) {
+            biome.setBiome(x,z, Biome.OCEAN);
+            h = (int) (1 + y + -1 * (Math.sqrt(Math.abs(biome_noise.noise(chunk_x * 16 + x, chunk_z * 16 + z)) * Math.abs(d_noise))));
             chunk.setBlock(x, h - 1, z, Material.SAND);
             chunk.setBlock(x, h - 2, z, Material.SAND);
-            for (int i = h; i < CCGen.minHeight + 1; i++) {
-                chunk.setBlock(x, i, z, Material.AIR);
-            }
-        }
-        if (c_noise < -0.15) {
-            biome.setBiome(x,z, Biome.DEEP_OCEAN);
-            h = (int) (y + (-1 * ((Math.abs(d_noise) * 30))));
-            chunk.setBlock(x, h - 1, z, Material.SAND);
-            for (int i = h; i < CCGen.minHeight + 1; i++) {
+            for (int i = h; i < ocean_limit + 1; i++) {
                 chunk.setBlock(x, i, z, Material.AIR);
             }
         }
         h = CCGen.calculateMaxHeight(x, z, chunk);
+
+        if (h <= deep_ocean_limit) {
+            biome.setBiome(x,z, Biome.DEEP_OCEAN);
+            h = (int) ((d_draw_noise.noise(chunk_x * 16 + x, chunk_z * 16 + z)) * 10 + h);
+            chunk.setBlock(x, h - 1, z, Material.SAND);
+            chunk.setBlock(x, h - 2, z, Material.SAND);
+            for (int i = h; i < ocean_limit + 1; i++) {
+                chunk.setBlock(x, i, z, Material.AIR);
+            }
+        }
+
+        h = CCGen.calculateMaxHeight(x, z, chunk);
         if (h < ocean_limit) {
-            for (int i = h + 1; i < CCGen.minHeight; i++) {
+            for (int i = h + 1; i < ocean_limit; i++) {
                 chunk.setBlock(x, i, z, Material.WATER);
             }
         }
